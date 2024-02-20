839ae625c5d02c0614c194f1241f26e32aafc31a
Mehmet Öner Yalçın <oneryalcin@gmail.com>
2024-02-20 14:07:44 +0000

    Change URL

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     description='Lemlist API client for Python.',
     author='Mehmet Oner Yalcin',
     author_email='oneryalcin@gmail.com',
-    url="https://github.com/the-friedkins/lemlist",
+    url="https://github.com/oneryalcin/lemlist",
     packages=['lemlist'],
     install_requires=['requests>=2.28.0', "tenacity>=8.2.3"],
 )
