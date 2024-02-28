4edd000853254f947310b660ed99f3f2f48b3224
Mehmet Öner Yalçın <oneryalcin@gmail.com>
2024-02-28 00:57:58 +0000

    Add get_leads and delete_campaign methods
    Two new methods are added: get_leads which retrieves a list of leads for a given campaign, and delete_campaign which deletes a campaign given its ID. Also, updates are made to ensure that API key from environment variables, and exception handling is implemented to prevent JSONDecodeErrors when receiving responses from the API.

--- lemlist/lemlist.py
+++ lemlist/lemlist.py
@@ -1,7 +1,8 @@
-
 import base64
 import json
+import csv
 from typing import Optional
+from hashlib import md5
 
 import requests
 from pydantic import BaseModel
@@ -43,7 +44,10 @@ class Client:
         response = requests.request(method, url, headers=self.headers, params=params, data=payload)
         response.raise_for_status()
 
-        return response.json()
+        try:
+            return response.json()
+        except json.JSONDecodeError:
+            return response.text
 
 
 class Campaigns:
@@ -68,9 +72,46 @@ class Campaigns:
         else:
             raise ValueError("Either campaign_id or campaign_name must be provided.")
 
-    def add_a_lead(self, campaign_id, lead: dict, deduplicate=True, find_email=False, linkedin_enrichment=False, verify_email=False):
+    def get_leads(self, campaign_id):
+        """Get leads for a campaign.
 
-        email = lead.pop("email")
+        GET https://api.lemlist.com/api/campaigns/:campaignId/export/leads?state=all
+        """
+        leads = self.client.send_request("GET", f"/campaigns/{campaign_id}/export/leads?state=all")
+
+        lines = leads.strip().split('\r\n')
+
+        # Create a CSV reader to process the lines, assuming the first line contains headers
+        reader = csv.reader(lines)
+
+        # Extract headers (column names)
+        headers = next(reader)
+
+        # Initialize a list to hold dictionaries created from rows
+        json_list = []
+
+        # Iterate over the remaining lines in the reader to construct dictionaries
+        # Each dictionary maps headers (column names) to the corresponding row values
+        for row in reader:
+            row_dict = {headers[i]: row[i] for i in range(len(headers))}
+
+            vals_for_hash = [v for k, v in row_dict.items() if k not in ('hash', 'emailStatus')]
+            # create a hash field for each row, takes all values and creates a md5 hash
+            row_dict['hash'] = md5(json.dumps(vals_for_hash).encode()).hexdigest()
+
+            _ = row_dict.pop('emailStatus')
+
+            json_list.append(row_dict)
+
+        return json_list
+
+    def delete_campaign(self, campaign_id):
+        return self.client.send_request("DELETE", f"/campaigns/{campaign_id}")
+
+    def add_a_lead(self, campaign_id, lead: dict, deduplicate=True, find_email=False, linkedin_enrichment=False,
+                   verify_email=False):
+
+        email = lead.get('email')
         if not email:
             raise ValueError("Email is required for a lead")
 
@@ -90,32 +131,36 @@ class Campaigns:
 
 
 # if __name__ == '__main__':
-#     client = Client(api_key="API_KEY_HERE")
+#     api_key = os.getenv("LEMLIST_API_KEY")
+#     client = Client(api_key=api_key)
 #     lemlist_campaigns = Campaigns(client=client)
 #     # c_list = lemlist_campaigns.get_campaigns()
 #     # # print(c_list)
 #
-#     campaign = lemlist_campaigns.get_campaign(campaign_name="Copilot Outbound Reach")
+#     campaign = lemlist_campaigns.get_campaign(campaign_name="Copilot Outbound Test")
 #     print(campaign)
 #
-#     l = {
-#         "email": "jdoe@acmeinc.com",
-#         "firstName": "John",
-#         "lastName": "Doe",
-#         "companyName": "Acme Inc.",
-#         "icebreaker": "Hey how do you do?",
-#         "phone": None,
-#         "picture": None,
-#         "linkedinUrl": "https://www.linkedin.com/jdoe",
-#         "companyDomain": "acmeinc.com",
-#         "g2_main_sector": "Cartoons",
-#         "g2_sub_sector": "Looney Tunes",
-#         "sender_name": "Alba"
-#     }
-#
-#     lead = Lead(**l)
-#
-#     response = lemlist_campaigns.add_a_lead(campaign_id=campaign["_id"], lead=lead.dict())
+#     leads = lemlist_campaigns.get_leads(campaign_id=campaign['_id'])
+#     print
+#     #
+#     # l = {
+#     #     "email": "jdoe@acmeinc.com",
+#     #     "firstName": "John",
+#     #     "lastName": "Doe",
+#     #     "companyName": "Acme Inc.",
+#     #     "icebreaker": "Hey how do you do?",
+#     #     "phone": None,
+#     #     "picture": None,
+#     #     "linkedinUrl": "https://www.linkedin.com/jdoe",
+#     #     "companyDomain": "acmeinc.com",
+#     #     "g2_main_sector": "Cartoons",
+#     #     "g2_sub_sector": "Looney Tunes",
+#     #     "sender_name": "Alba"
+#     # }
+#     #
+#     # lead = Lead(**l)
+#     #
+#     # response = lemlist_campaigns.add_a_lead(campaign_id=campaign["_id"], lead=lead.dict())
 #
 #     print(response)
 
